b4cbef3faa71151ec99fe81cedec0b1295856859
payneda <payneda@my.wccc.edu>
2015-09-05 18:18:05 -0400

    Daniel Payne Assignment 2
    This my first attempt

---
+++ danielpayneassignment2/src/danielpayneassignment2/Danielpayneassignment2.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the tem[plate in the editor.
+ */
+package danielpayneassignment2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Inspiron
+ */
+public class Danielpayneassignment2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args, int userNumber) {
+        // TODO code application logic here
+    
+    Scanner keyboard = new Scanner(System.in);
+    int number = keyboard.nextInt();
+    count = 0
+    userNumber = 5;
+    }if (userNumber < 5){
+        System.out.println("Number is low");
+    }if else (userNumber > 5){
+        System.out.println("Number is high");
+    }if (userNumber = 5){
+        System.out.println("Number is Correct");
+        }if{ (Count = 0)
+                count = count + 1
+        }if else{ (count = 10)
+                System.out.println("Game Over");
+        
+        
+        
+  
+    }
+    
+}
